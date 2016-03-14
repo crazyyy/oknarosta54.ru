0ad50fc47db6088ba805b0032d85095882ea695f
crazyyy <crazyyy@gmail.com>
2016-03-14 17:36:34 +0200

    page readys

--- wordpress/wp-content/themes/wp-oknarosta/page.php
+++ wordpress/wp-content/themes/wp-oknarosta/page.php
@@ -1,18 +1,22 @@
 <?php get_header(); ?>
-  <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-      <h1 class="page-title inner-title"><?php the_title(); ?></h1>
-      <?php the_content(); ?>
-      <?php edit_post_link(); ?>
-
-    </article>
-  <?php endwhile; else: // If 404 page error ?>
-    <article>
-
-      <h2 class="page-title inner-title"><?php _e( 'Sorry, nothing to display.', 'wpeasy' ); ?></h2>
-
-    </article>
-  <?php endif; ?>
-<?php get_sidebar(); ?>
+  <div id="title" class="titlebar clearfix">
+    <div class="container_16">
+      <div class="grid_16">
+        <?php if (function_exists('easy_breadcrumbs')) easy_breadcrumbs(); ?>
+      </div>
+    </div>
+  </div>
+  <div class="container_16 clearfix">
+    <div id="content" class="grid_16 fullwidth " role="main">
+      <article id="post-150" class="post-150 page type-page status-publish hentry">
+        <header class="entry-header clearfix">
+          <h1 class="entry-title"><?php the_title(); ?></h1>
+        </header>
+        <span class="entry-meta entry-meta-middle"></span>
+        <div class="entry-content clearfix">
+          <?php the_content(); ?>
+        </div><!-- entry-content -->
+      </article>
+    </div>
+  </div><!-- #container -->
 <?php get_footer(); ?>
