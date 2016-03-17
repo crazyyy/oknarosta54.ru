e28bec2f96b4dafdca96e8888a8faa963cea98a2
crazyyy <crazyyy@gmail.com>
2016-03-17 23:41:34 +0200

    clean unused

--- wordpress/wp-content/plugins/ml-post-slider/js/script.js
+++
@@ -1,7 +0,0 @@
-﻿<script type="text/javascript">
-	jQuery(document).ready(function() {
-		jQuery('.ml_post_slider').cycle({
-			fx: 'fade' // choose your transition type, ex: fade, scrollUp, shuffle, etc...
-		});
-	});
-</script>
