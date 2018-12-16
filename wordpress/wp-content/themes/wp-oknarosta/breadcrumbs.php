933fd845f076695b211127ec487b9d8e53a01c1d
Vitalii Antoniuk <va@jelasric.com>
2018-12-16 23:35:56 +0200

    close and archive project

--- wordpress/wp-content/themes/wp-oknarosta/breadcrumbs.php
+++
@@ -1,7 +0,0 @@
-<div id="title" class="titlebar clearfix">
-  <div class="container_16">
-    <div class="grid_16">
-      <?php if (function_exists('easy_breadcrumbs')) easy_breadcrumbs(); ?>
-    </div>
-  </div>
-</div>
